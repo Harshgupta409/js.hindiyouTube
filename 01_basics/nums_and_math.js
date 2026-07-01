1a99077890ec1163b8fc4a49e0a986531d92449c
Harshgupta409 <hboss4111@gmail.com>
2026-07-01 19:51:09 +0000

    container commit done

---
+++ 01_basics/nums_and_math.js
@@ -0,0 +1,20 @@
+const score=400
+console.log(score);
+const balance =new Number(100)
+console.log(balance);
+  
+
+console.log(balance.toString());
+console.log(balance.toFixed(2));
+
+const othernumber=123.892541
+console.log(othernumber.toPrecision(3));
+
+const hundreds=1000000
+
+console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++maths+++++++++++++++++
+console.log(Math);
+
+
